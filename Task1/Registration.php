5c2fdb6636e488d292a0e71978b7fe90b6b7b6d8
Zanibul-Islam <84762281+Zanibul-Islam@users.noreply.github.com>
2021-05-31 20:54:15 +0600

    Add files via upload

---
+++ Task1/Registration.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html>
+    <head>
+
+        <title>Registration</title>
+
+</head>
+        <body>
+
+                <h2> Registration Form</h2>
+<form>
+    <table>
+     <tr>
+     <td>
+                Full Name:
+     </td>
+     <td>
+                <input type="text">
+     </td>
+     </tr>  
+     <tr>
+      <td>
+                Email: 
+     </td>
+     <td>
+                <input type="text">
+     </td>
+     </tr>
+     <tr>
+      <td>
+                Password: 
+     </td>
+     <td>
+                <input type="password">
+     </td>
+     <tr>
+            
+           
+    </tr>
+    <td>
+                Comment:
+    </td>
+    <td>
+                <textarea name="" id="" cols="25" rows="3">
+                </textarea>
+   </td>
+   </tr>
+   <tr>
+   <td></td>
+             
+    </tr>
+       
+    <tr>
+    <td> 
+                   Gender: 
+     </td>
+     <td>   
+                        <input type="radio" name= "gender"><label>Female</label>
+                        <input type="radio" name="gender"><label>Male</label>
+                        <input type="radio" name="gender"><label>Other</label>
+     </td>
+               
+     </tr>
+       <tr>
+      <td>             Please choose a hobby:
+      </td>
+      <td> 
+                        <input type="radio" name= "hobby">Singing
+                        <input type="radio" name="hobby">Dancing
+                        <input type="radio" name="hobby">Reading
+                               </td>
+        <tr>
+<td>
+<label >Please Choose a file</label>
+</td>
+
+                <td>
+                <input type="button" value="Choose file ">
+                <label> no file choosen</label>
+                </td>
+            </tr>
+                
+        </tr>
+        <tr>
+               <td>
+               <input type="submit" value="Submit">
+               <input type="reset">
+               </td>
+        </tr>
+        
+    </table>
+        </form>
+
+    </body>
+</html>
